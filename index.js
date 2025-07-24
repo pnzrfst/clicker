9e52e84fea4002d7baad72b0b28cc4b187502142
luiz <144607216+pnzrfst@users.noreply.github.com>
2025-07-23 22:02:25 -0300

    subindo

--- index.js
+++ index.js
@@ -1,51 +1,69 @@
-let qtdClicks = 0;
+let clicksQuantity = 0;
+let coins = 0;
 
 const stats = {
-    qtdClicks : 0,
+    clicksQuantity : 0,
     earnedCoins : 0,
     coinsPerMinutes : 0
 };
 
-let coins = 0;
-
-
-////// aumenta as coins baseado no true e na qtd que o booster fornece
-
-
-function growCoinsPerClick(isActive, booster){
-    if(isActive == true){
+// aumenta as coins baseado no true e na qtd que o booster fornece
+function growCoinsPerBooster(isActive, booster) {
+    if (isActive) {
         const multipleCoins = coins * booster;
         stats.earnedCoins++;
-    }else{
+    } else {
         return coins;
     }
 }
 
-
-///////////////captura o clique, aumenta o coin e mostra na tela
-document.getElementById("main-button").addEventListener("click", ()=>{
+// captura o clique, aumenta o coin e mostra na tela
+document.getElementById("main-button").addEventListener("click", () => {
     coins++;
-    const spanClick = document.getElementById("cliques");
-    stats.qtdClicks++;
-    stats.earnedCoins++;
-    qtdClicks.innerText = qtdClicks;
+    clicksQuantity++;
+
+    stats.clicksQuantity = clicksQuantity;
+    stats.earnedCoins = coins;
+
+    const qtdClicks = document.getElementById("cliques");
+    qtdClicks.innerText = clicksQuantity;
 });
 
+// coleta coins automaticamente
+function collectCoinsAutomatically(isActive) {
+    if (!isActive) return;
 
-/////////////////// coleta coins automaticamente
-
-let intervalID = null;
-
-function collectCoinsAutomatically(isActve){
-    if(isActve && intervalID === null){
-        intervalID = setInterval(() => {
-            coins++;
-            stats.earnedCoins++;
-        }, 1000);
-    }else{
-       return intervalID;
-    }
+    setInterval(() => {
+        coins++;
+        stats.earnedCoins = coins;
+    }, 1000);
 }
 
+function showStats(isActive){
+    if(!isActive){
+        return null;
+    }
+    
+    const statsArea = document.createElement("div");
+    
+    
+    const totalOfClicks= document.createElement("h3");
+    totalOfClicks.className = "menu-button";
+    totalOfClicks.innerText = stats.clicksQuantity;
+    
+    const totalOfCoinsEarned = document.createElement("h3");
+    totalOfCoinsEarned.className = "menu-button";
+    totalOfCoinsEarned.innerText = stats.coinsPerMinutes;
+    
+    const totalCoinsPerMinutes = coins / 60;
+    const showTotalCoinsPerMinutes = document.createElement("h3");
+    showTotalCoinsPerMinutes.innerText = totalCoinsPerMinutes;
+    showTotalCoinsPerMinutes.className = "menu-button";
+    
+    statsArea.appendChild(totalOfClicks);
+    statsArea.appendChild(totalOfCoinsEarned);
+    statsArea.appendChild(showTotalCoinsPerMinutes);
+    
+}
 
 
